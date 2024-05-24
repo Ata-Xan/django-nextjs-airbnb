50ee63d227ff2bde0c8533686d785af831a0a32a
ata xan <ataa1370@gmail.com>
2024-05-24 13:02:06 +0200

    backend connection is established, list item is done

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,13 +1,42 @@
+'use client'
 import PropertyItem from './PropertyItem';
-const PropertyList = ()=>{
+import { useEffect, useState } from 'react';
+
+export type PropertyType = {
+    id: string;
+    title: string;
+    price_per_night: number;
+    image_url: string;
+}
+
+
+const PropertyList = () => {
+    const [properties, setProperties] = useState<PropertyType[]>([]); // [{}
+    const getProperties = async () => {
+        const url = 'http://localhost:8000/api/properties/';
+        const response = await fetch(url, { method: 'GET' })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('json', json)
+                setProperties(json.data)
+
+            })
+            .catch((error) => {
+                console.error('error:', error)
+
+                
+            });
+    };
+
+    useEffect(() => {
+        getProperties();
+    }, []);
+
     return (
         <div className="mt-4 grid gird-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
-            <PropertyItem/>
-            <PropertyItem/>
-            <PropertyItem/>
-            <PropertyItem/>
-            <PropertyItem/>
-            <PropertyItem/>
+            {properties.map((property) => 
+                (<PropertyItem key={property.id} property= {property}  />)
+            )}
         </div>
     );
 }
