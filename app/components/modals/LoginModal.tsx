c65f4579d2c5c44aa8219731c9d3719e0a52a393
ata xan <ataa1370@gmail.com>
2024-05-26 11:11:09 +0200

    login is done

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,40 +1,69 @@
 'use client';
 
 import Modal from './Modal';
+import { useRouter } from 'next/navigation';
 import useLoginModal from '../../hooks/useLoginModal';
 import CustomButton from '../forms/CustomButton';
-
+import { handleLogin } from '../../lib/actions';
+import { useState } from 'react';
+import apiService from '@/app/services/apiService';
 
 const LoginModal = () => {
-    const loginModal = useLoginModal();
+  const router = useRouter();
+  const loginModal = useLoginModal();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<string[]>([]);
 
-    const content = (
-        <>
-          <form className="space-y-4">
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-full p-2 h-[54px] border border-gray-300 rounded-xl"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-            <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                The email or password you entered is incorrect. Please try again.
-            </div>
-            <CustomButton label="Submit" onClick={() => console.log("clicked button")} />
-    
-          </form>
-        </>
-      );
+  const submitLogin = async () => {
+    const formData = {
+      email: email,
+      password: password
+    }
+    const response = await apiService.post('/api/auth/login/', JSON.stringify(formData));
+
+    if (response.access) {
+      handleLogin(response.user_id, response.access, response.refresh);
+      loginModal.close();
+      router.push('/');
+    } else {
+      // non_field_errors is a key in the response object that contains an array of errors which was returned by the server
+      setErrors(response.non_field_errors);
+    }
+  }
+
+  const content = (
+    <>
+      <form
+        action={submitLogin}
+        className="space-y-4">
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          className="w-full p-2 h-[54px] border border-gray-300 rounded-xl"
+        />
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          className="w-full p-2 border border-gray-300 rounded"
+        />
+        {errors.map((error, index) => (
+          <div key={`error_${index}`} className="text-red-500 text-sm">{error}</div>
+        ))}
+        <CustomButton label="Submit" onClick={() => submitLogin()} />
 
 
-    return (
-       <Modal isOpen={loginModal.isOpen} close={loginModal.close} label={"Log in"} content={content} />
-        
-    )
+      </form>
+    </>
+  );
+
+
+  return (
+    <Modal isOpen={loginModal.isOpen} close={loginModal.close} label={"Log in"} content={content} />
+
+  )
 }
 
 export default LoginModal;
